6170c4087a5456e93bab3e9a4dffb9cc455f6d20
WilliamLebris <williamtissi2006@gmail.com>
2025-05-07 14:55:04 -0700

    Remove build directory from repo

--- build/CMakeFiles/Proj1Lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libProj1Lib.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/williamtissi/Desktop/Proj1
-# Build directory: /Users/williamtissi/Desktop/Proj1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/williamtissi/Desktop/Proj1/build/test_orders[1]_include.cmake")
-subdirs("_deps/googletest-build")
